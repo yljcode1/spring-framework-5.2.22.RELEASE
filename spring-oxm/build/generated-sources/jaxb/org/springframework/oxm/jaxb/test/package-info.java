736de8500d76801660b452371d248a8ecb80e0da
yaolijun <yaolijun@bintools.cn>
2023-04-03 09:28:19 +0800

    fix(all):不知道什么原因，导致一千多个文件修改

--- spring-oxm/build/generated-sources/jaxb/org/springframework/oxm/jaxb/test/package-info.java
+++ spring-oxm/build/generated-sources/jaxb/org/springframework/oxm/jaxb/test/package-info.java
@@ -2,7 +2,7 @@
 // 此文件是由 JavaTM Architecture for XML Binding (JAXB) 引用实现 v2.3.1 生成的
 // 请访问 <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
 // 在重新编译源模式时, 对此文件的所有修改都将丢失。
-// 生成时间: 2023.02.21 时间 03:51:21 PM CST 
+// 生成时间: 2023.04.01 时间 10:01:54 PM CST 
 //
 
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://samples.springframework.org/flight", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
